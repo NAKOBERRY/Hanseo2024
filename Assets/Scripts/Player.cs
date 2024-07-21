96150809ce13c670a3b9428c1ad6c221552b0785
NAKOBERRY <tt01033135063@gmail.com>
2024-07-21 15:47:41 +0900

    플레이어 스크립트 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rigid;
-
-    public Vector2 inputVec;
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
+    private Vector2 inputVec;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpPower = 10f;
+    private bool isGrounded;
 
     private void Start()
     {
@@ -18,27 +16,43 @@ public class Player : MonoBehaviour
     private void Update()
     {
         inputVec.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
         PlayerMove();
-        Jump();
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigid.velocity = Vector2.zero;
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        }
-
+        rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
     }
 
     private void PlayerMove()
     {
-        var newInputVec = inputVec.normalized * speed * Time.deltaTime;
+        var newInputVec = inputVec * speed * Time.deltaTime;
         rigid.MovePosition(rigid.position + newInputVec);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
