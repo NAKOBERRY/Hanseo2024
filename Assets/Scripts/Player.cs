5e4094712b1216cd79f93a6bf6c8fa40c427292f
NAKOBERRY <tt01033135063@gmail.com>
2024-07-21 15:41:11 +0900

    플레이어 컨트롤

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rigid;
+
+    public Vector2 inputVec;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void PlayerMove()
+    {
+        var newInputVec = inputVec.normalized * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + newInputVec);
     }
 }
