991ae5ac4bd64dc1edc12b6d0cba667f43faf529
NAKOBERRY <tt01033135063@gmail.com>
2024-07-24 20:30:13 +0900

    서클 생성(작업중)

---
+++ Assets/Scripts/CircleRogic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CircleRogic : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    [SerializeField] private int speed = 3;
+    [SerializeField] private GameObject circle;
+
+    private void OnEnable()
+    {
+        rigid = circle.GetComponent<Rigidbody2D>();      
+    }
+
+
+
+    private void SpawnCircle(Vector2 spawnPos, Vector2 spawnDir)
+    {
+        Instantiate(circle, spawnPos, Quaternion.identity);
+        rigid.velocity = spawnDir * speed;
+    }
+}
